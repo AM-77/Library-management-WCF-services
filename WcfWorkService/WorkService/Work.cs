3f767505dcb47f08289c1c0db16a2c7437e01bad
Mohamed Amine Griche <amine.griche77@gmail.com>
2019-01-04 14:33:13 +0100

    Added the Work model.

---
+++ WcfWorkService/WorkService/Work.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace WorkService
+{
+    [DataContract]
+    public class Work
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string title { get; set; }
+        [DataMember]
+        public string author { get; set; }
+        [DataMember]
+        public string theme {get; set;}
+        [DataMember]
+        public string keywords { get; set; }
+        [DataMember]
+        public string barecode { get; set; }
+        [DataMember]
+        public string type { get; set; }
+        [DataMember]
+        public int reserved { get; set; }
+
+        public Work() { }
+
+        public Work(int id, string title, string author, string theme, string keywords, string barecode, string type, int reserved)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.theme = theme;
+            this.keywords = keywords;
+            this.barecode = barecode;
+            this.type = type;
+            this.reserved = reserved;
+        }
+
+    }
+}
