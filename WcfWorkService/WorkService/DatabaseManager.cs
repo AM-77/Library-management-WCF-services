327acddf69c17106c39bc4a507b9821e44826c5b
Mohamed Amine Griche <amine.griche77@gmail.com>
2019-01-04 14:31:56 +0100

    Added database manager class.

---
+++ WcfWorkService/WorkService/DatabaseManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MySql.Data.MySqlClient;
+
+namespace WorkService
+{
+    public class DatabaseManager
+    {
+        public string db_host = "localhost";
+        public string db_name = "mini_projet_poc";
+        public string db_user = "root";
+        public string db_password = "";
+
+        private MySqlConnection db_connection = null;
+
+        public DatabaseManager() { }
+
+        public MySqlConnection connect()
+        {
+            if (!is_connected())
+            {
+                this.db_connection = new MySqlConnection("SERVER=" + db_host + ";DATABASE=" + db_name + ";UID=" + db_user + ";PASSWORD=" + db_password + ";");
+                this.db_connection.Open();
+            }
+
+            return this.db_connection;
+        }
+
+        public bool is_connected()
+        {
+
+            if (this.db_connection != null)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public void close()
+        {
+            if (this.db_connection != null)
+            {
+                this.db_connection.Close();
+                this.db_connection = null;
+            }
+        }
+    }
+}
