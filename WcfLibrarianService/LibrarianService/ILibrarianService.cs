b792e17bd205ee162877e78263bbc4fb6e56517c
Mohamed Amine Griche <amine.griche77@gmail.com>
2019-01-06 19:43:56 +0100

    Added database manager class and service interface.

---
+++ WcfLibrarianService/LibrarianService/ILibrarianService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LibrarianService
+{
+    [ServiceContract]
+    public interface ILibrarianService
+    {
+        [OperationContract]
+        bool deblock(string id_client);
+
+        [OperationContract]
+        bool confirm_reservation(string id_client, int id_work);
+
+    }
+
+}
