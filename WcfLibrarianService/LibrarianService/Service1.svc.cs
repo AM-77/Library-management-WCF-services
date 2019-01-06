e250d1eedd27c69c49572f9e00bfcf8cf8d5a83f
Mohamed Amine Griche <amine.griche77@gmail.com>
2019-01-06 19:45:09 +0100

    Added confirm reservation func.

---
+++ WcfLibrarianService/LibrarianService/Service1.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace LibrarianService
+{
+    public class Service1 : ILibrarianService
+    {
+        public DatabaseManager db_manager = new DatabaseManager();
+        public DatabaseManager db_manager1 = new DatabaseManager();
+
+        public bool confirm_reservation(string id_client, int id_work)
+        {
+            MySqlConnection connection = db_manager.connect();
+
+            string query = "SELECT * FROM `reservation` ;";
+            var cmd = new MySqlCommand(query, connection);
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read() && reader != null)
+            {
+                System.Diagnostics.Debug.WriteLine(id_work + " " + reader.GetInt32(1));
+
+                if (reader["id_client"].Equals(id_client) && reader.GetInt32(1) == id_work)
+                {
+                    MySqlConnection connection1 = db_manager1.connect();
+                    string query1 = "UPDATE `reservation` SET `confirmed` = '1' WHERE `reservation`.`id` = '" + reader.GetInt32(0) + "' ;";
+                    var cmd1 = new MySqlCommand(query1, connection1);
+                    var reader1 = cmd1.ExecuteNonQuery();
+                    db_manager1.close();
+                    db_manager.close();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
